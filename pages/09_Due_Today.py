1467fc01cf97b38cf6a48fadbbdb87c2dfefba94
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 20:25:44 +1000

    Update 09_Due_Today.py

--- pages/09_Due_Today.py
+++ pages/09_Due_Today.py
@@ -7,18 +7,6 @@ st.title("Due Today Debug")
 
 df = load_backlog()
 
-st.write("Columns")
-st.write(df.columns.tolist())
-
-st.write("Bklg.Qty Raw Values")
-st.write(df["Bklg.Qty"].head(20))
-
-st.write("Bklg.Qty Data Type")
-st.write(df["Bklg.Qty"].dtype)
-
-st.write("Unique Sample Values")
-st.write(df["Bklg.Qty"].dropna().head(50).tolist())
-
 st.write(
     df[
         ["Material", "Bklg.Qty"]
