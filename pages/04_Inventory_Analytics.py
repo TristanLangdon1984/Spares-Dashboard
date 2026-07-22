ac2604c63a578f3e8ef447f1f36d24b9df669810
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 11:08:54 +1000

    Update 04_Inventory_Analytics.py

--- pages/04_Inventory_Analytics.py
+++ pages/04_Inventory_Analytics.py
@@ -1,2 +1,28 @@
 import streamlit as st
-st.title('04 Inventory Analytics')
+import plotly.express as px
+
+from utils.loader import load_parts
+
+st.title("Inventory Analytics")
+
+df = load_parts()
+
+top = df.nlargest(
+    20,
+    "Total conspt"
+)
+
+fig = px.bar(
+    top,
+    x="Total conspt",
+    y="Material Description",
+    orientation="h",
+    title="Top Consumed Parts"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+st.dataframe(top)
