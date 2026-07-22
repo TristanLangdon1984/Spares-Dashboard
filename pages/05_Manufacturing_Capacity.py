f73fc19d927a8547dc355a14d54b25d9bf059e18
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 11:04:26 +1000

    Update 05_Manufacturing_Capacity.py

--- pages/05_Manufacturing_Capacity.py
+++ pages/05_Manufacturing_Capacity.py
@@ -1,2 +1,34 @@
 import streamlit as st
-st.title('05 Manufacturing Capacity')
+import plotly.express as px
+
+from utils.loader import load_metrics
+
+st.title("Operations Dashboard")
+
+df = load_metrics()
+
+fig = px.bar(
+    df,
+    x="Date",
+    y=["Raised","Produced"],
+    barmode="group",
+    title="Orders Raised vs Produced"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+fig2 = px.line(
+    df,
+    x="Date",
+    y="Overproduction Rate",
+    markers=True,
+    title="Overproduction Rate"
+)
+
+st.plotly_chart(
+    fig2,
+    use_container_width=True
+)
