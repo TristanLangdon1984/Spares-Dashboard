c493560f71a68fcfe90ed866d3b6460652f2b4c8
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 17:44:46 +1000

    Remove unnecessary write statements for columns and shape

--- pages/99_Column_Check.py
+++ pages/99_Column_Check.py
@@ -1,15 +1,12 @@
 import streamlit as st
+
 from utils.loader import load_backlog
 
 st.title("Column Check")
 
 df = load_backlog()
 
-st.write("Columns")
-
 st.write(df.columns.tolist())
-
-st.write("Shape")
 
 st.write(df.shape)
 
