b770b69349edc071f33c42e561b6fda2b85b3ac1
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 17:57:23 +1000

    Change CSV encoding and strip column names
    Updated encoding from 'utf-16' to 'latin1' for CSV loading and added column stripping.

--- utils/loader.py
+++ utils/loader.py
@@ -6,21 +6,27 @@ MB51_FILE = "data/MB51 - MEL_SPARES.XLS"
 
 def load_backlog():
 
-    return pd.read_csv(
+    df = pd.read_csv(
         BACKLOG_FILE,
         sep="\t",
-        encoding="utf-16",
-        encoding_errors="ignore",
+        encoding="latin1",
         engine="python"
     )
+
+    df.columns = df.columns.str.strip()
+
+    return df
 
 
 def load_mb51():
 
-    return pd.read_csv(
+    df = pd.read_csv(
         MB51_FILE,
         sep="\t",
-        encoding="utf-16",
-        encoding_errors="ignore",
+        encoding="latin1",
         engine="python"
     )
+
+    df.columns = df.columns.str.strip()
+
+    return df
