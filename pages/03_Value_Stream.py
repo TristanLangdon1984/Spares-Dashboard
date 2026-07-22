cce78c62bef0259dbe87e93a57b35172715ef032
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 11:27:23 +1000

    Update 03_Value_Stream.py

--- pages/03_Value_Stream.py
+++ pages/03_Value_Stream.py
@@ -1,2 +1,27 @@
-import streamlit as st
-st.title('03 Value Stream')
+import pandas as pd
+
+
+def classify_product(description):
+
+    desc = str(description).upper()
+
+    if "PELORIS" in desc:
+        return "PELORIS"
+
+    if "CER" in desc:
+        return "CER"
+
+    if "ARC" in desc:
+        return "ARC"
+
+    if (
+        "BOND" in desc
+        or "RX" in desc
+        or "PRIME" in desc
+        or "BOND-III" in desc
+        or "BOND III" in desc
+        or "BOND-MAX" in desc
+    ):
+        return "BOND"
+
+    return "OTHER"
