fcd0ab504bd6cd30229daba3a3b2a73e2764fd11
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 18:38:10 +1000

    Update Value Stream Dashboard display logic

--- pages/03_Value_Stream.py
+++ pages/03_Value_Stream.py
@@ -1,13 +1,7 @@
 import streamlit as st
-
 from utils.loader import load_backlog
-
-st.title("Value Stream Dashboard")
 
 df = load_backlog()
 
 st.write(df.shape)
-
 st.write(df.columns.tolist())
-
-st.dataframe(df.head())
