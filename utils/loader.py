9cda715c8862af9e9cd95fd5f2a4bded73dde9c2
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 18:18:09 +1000

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,9 +1,3 @@
-import pandas as pd
-
-BACKLOG_FILE = "data/ZVREP386 - 3013BACKLOG1.XLS"
-MB51_FILE = "data/MB51 - MEL_SPARES.XLS"
-
-
 def load_backlog():
 
     df = pd.read_csv(
@@ -13,20 +7,12 @@ def load_backlog():
         engine="python"
     )
 
-    df.columns = df.columns.str.strip()
-
-    return df
-
-
-def load_mb51():
-
-    df = pd.read_csv(
-        MB51_FILE,
-        sep="\t",
-        encoding="latin1",
-        engine="python"
+    df.columns = (
+        df.columns
+        .str.strip()
+        .str.replace("\n", " ", regex=False)
+        .str.replace("\r", " ", regex=False)
+        .str.replace("  ", " ", regex=False)
     )
-
-    df.columns = df.columns.str.strip()
 
     return df
