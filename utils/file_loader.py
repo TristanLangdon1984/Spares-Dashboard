99abb4a68a5f553548deb1e6107507c4a0b4ae12
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 15:47:51 +1000

    Fix file paths and update function names in file_loader

--- utils/file_loader.py
+++ utils/file_loader.py
@@ -1,31 +1,43 @@
 from pathlib import Path
-*BACKLOG_FOLDER = Path(
-    r"C:\Us*rs\ztl\Danaher\LBSM*L Production - Documents\Spares\QD*P data\Daily SAP Backlog Export"
-)*
+
+BACKLOG_FOLDER = Path(
+    r"C:\Users\ztl\Danaher\LBSMEL Production - Documents\Spares\QDIP data\Daily SAP Backlog Export"
+)
+
 MB51_FOLDER = Path(
-    r"C:\User*\ztl\Danaher\LBSMEL Production - D*cuments\Spares\QDIP data\Daily SAP*Productivity Export"
+    r"C:\Users\ztl\Danaher\LBSMEL Production - Documents\Spares\QDIP data\Daily SAP Productivity Export"
 )
 
 
-def get_l*test_back*og():
+def get_latest_backlog():
 
     files = list(
         BACKLOG_FOLDER.glob("*.xls*")
     )
 
+    if len(files) == 0:
+        raise FileNotFoundError(
+            f"No Excel files found in {BACKLOG_FOLDER}"
+        )
+
     return max(
-    *   files,
-        key=lambda x: x.*tat().st_mtime
+        files,
+        key=lambda x: x.stat().st_mtime
     )
 
 
-def get_lat*st_mb51():
+def get_latest_mb51():
 
     files = list(
-    *   MB51*FOLDER.glob("*.xls*")
+        MB51_FOLDER.glob("*.xls*")
     )
 
+    if len(files) == 0:
+        raise FileNotFoundError(
+            f"No Excel files found in {MB51_FOLDER}"
+        )
+
     return max(
-        *iles,
-        key=lambda x: x.stat*).st_mtime
+        files,
+        key=lambda x: x.stat().st_mtime
     )
