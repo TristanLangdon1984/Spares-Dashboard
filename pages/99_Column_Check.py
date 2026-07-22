c1831e92c583a953db99b82b5bedad0838afd0ec
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 15:44:52 +1000

    Update 99_Column_Check.py

--- pages/99_Column_Check.py
+++ pages/99_Column_Check.py
@@ -1,36 +1,11 @@
 import streamlit as st
+from pathlib import Path
 
-from utils.file_loader import (
-    get_latest_backlog,
-    get_latest_mb51
-)
+st.write("Current Folder")
+st.write(Path.cwd())
 
-st.title("SAP File Check")
+st.write("Utils Exists?")
+st.write(Path("utils").exists())
 
-col1, col2 = st.columns(2)
-
-with col1:
-
-    try:
-        backlog = get_latest_backlog()
-
-        st.success("Backlog File Found")
-
-        st.code(str(backlog))
-
-    except Exception as e:
-
-        st.error(e)
-
-with col2:
-
-    try:
-        mb51 = get_latest_mb51()
-
-        st.success("MB51 File Found")
-
-        st.code(str(mb51))
-
-    except Exception as e:
-
-        st.error(e)
+if Path("utils").exists():
+    st.write(list(Path("utils").glob("*")))
