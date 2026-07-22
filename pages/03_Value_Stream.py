e527e287730d84b9867a2974609ac37911920fad
TristanLangdon1984 <Tristan.langdon@leicabiosystems.com>
2026-07-22 18:28:29 +1000

    Update 03_Value_Stream.py

--- pages/03_Value_Stream.py
+++ pages/03_Value_Stream.py
@@ -1,8 +1,13 @@
 import streamlit as st
+
 from utils.loader import load_backlog
 
 st.title("Value Stream Dashboard")
 
 df = load_backlog()
 
-st.write(df)
+st.write(df.shape)
+
+st.write(df.columns.tolist())
+
+st.dataframe(df.head())
